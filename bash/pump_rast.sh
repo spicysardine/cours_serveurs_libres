db4b9b433357335251da8d11333bb9b0040ddacf
beetroot <spicy.sardine@yandex.com>
2026-02-13 20:24:30 +0100

    Refactor raster processing script for parallel execution
    Refactor raster processing script for parallel execution

--- bash/pump_rast.sh
+++ bash/pump_rast.sh
@@ -1,95 +1,74 @@
 #!/bin/bash
 
-URL="https://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF"
-THREADS=$(nproc)
+OUTPUT_DATABASE='geofab'
+OUTPUT_SCHEMA='raster'
+OUTPUT_SRS=4326
 
+shopt -s extglob
 
-#preparation des tableaux
-COLUMNS=($(seq 10 26))
-ROWS=($(seq 1 4))
+# nettoyage du répertoire de travail
+rm +(*tif|*zip)
 
-##Méthode séquencielle (classique):
-for row in ${ROWS[@]}
-do
-	for column in ${COLUMNS[@]};
-	do
-		wget --no-check-certificate "${URL}/srtm_${column}_0${row}.zip" 2> out.log;
-	done
-done
+BASE_URL="https://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF/"
 
+ROWS=($(seq 1 5))
+COLS=($(seq 1 26))
 
-#Méthode parallèle:
-parallel -j $THREADS "wget --no-check-certificate ${URL}/srtm_{1}_0{2}.zip" ::: $(echo ${COLUMNS[@]}) ::: $(echo ${ROWS[@]})
+# approche séquentielle (lente)
+#for row in ${ROWS[@]}
+#do
+#	for col in ${COLS[@]}
+#	do
+#		col=$(printf "%02d\n" $col)
+#		row=$(printf "%02d\n" $row)
+#		wget --no-check-certificate \
+#			"${BASE_URL}srtm_${col}_${row}.zip"
+#	done;
+#done;
 
-# étapes
+#ls *zip | while read zipfile
+#	do
+#		unzip -j -o $zipfile "*tif" 
+#	done;
 
-# désarchivage des fichiers
-ls *zip | parallel -j $THREADS "unzip -j -o {} '*tif'"
+# approche de téléchargement en parallèle:
+parallel -j $(nproc) \
+	"wget --no-check-certificate ${BASE_URL}srtm_{1}_{2}.zip" \
+	::: $(printf "%02d\n" ${COLS[@]}) \
+	::: $(printf "%02d\n" ${ROWS[@]})
 
-
-## Mosaicage dans une couche virtuelle
-gdalbuildvrt canada.vrt *tif
-
-## Reprojection de la couche virtuelle dans un fichier GeoTIFF
-## La reprojection est obligatoire pour éviter d’obtenir des images corrompues
-## noter que les fichiers MNT SRTM ne sont pas projetés
-gdalwarp -t_srs EPSG:3348 -r bilinear canada.vrt canada.tif 
-
-## Obtenir des informations sur le fichier
-gdalinfo -mm canada.tif
-
-## Génération du fichier hillshade (reliefs ombragés)
-gdaldem hillshade -of GTIFF -az 315 -alt 45 canada.tif canada_hillshade.tif
-
-## Génération du fichier de reliefs colorés color-relief
-gdaldem color-relief canada.tif color_relief.txt -alpha canada_color_relief.tif
-
-## Génération du fichier de pente
-gdaldem slope canada.tif canada_slope.tif
-
-## Génération du fichier de pente coloré
-gdaldem color-relief -of GTIFF canada_slope.tif color_slope.txt -alpha canada_slopeshade.tif
+# desarchivage en masse
+ls *zip | parallel -j $(nproc) \
+	"unzip -j -o {} '*tif'" 
 
 
 
+## insertion d’un fichier raster en WGS84 dans la BDD à l’aide de raster2pgsql
+## avec tuilage automatique
+## -s: spécifier le CSRS
+## -I: mettre en place un index
+## -M: accompagner d’un vacuum pour ganger en espace de stockage
+## -C: ajouter des contraintes de type sur la colonne raster dans la BDD
+## -F: mettre l’intitulé du fichier comme nom de la colonne
+## -t: spécifier les dimensions du tuilage tuilage
 
+# approche séquentielle
+#raster2pgsql -s 4326 -I -M -C -F -t 500x500 *.tif | psql -d beetroot
 
+# approche parallèle d'insertion:
+ls *tif | parallel -j $(nproc) \
+"raster2pgsql -s ${OUTPUT_SRS} -I -M -C -F -t 500x500 ${OUTPUT_SCHEMA}.{} | psql -d ${OUTPUT_DATABASE}"
 
+# génératin de mosaique virtuelle
+gdalbuildvrt  canada.vrt *tif
 
+gdalwarp -of GTIFF -t_srs EPSG:3348 -r bilinear canada.vrt canada.tif
 
+gdaldem hillshade -of GTIFF canada.tif -az 315 -alt 45 canada_hillsade.tif
 
+gdaldem slope -of GTIFF canada.tif canada_slope.tif
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+gdaldem color-relief -of GTIFF canada.tif color_relief.txt -alpha canada_color_relief.tif
 
 
 
